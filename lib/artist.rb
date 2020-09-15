c03009f53cbcf10e7cf44e5b17ff4d478372c43d
Lesly Juarez <leslyj2@gmail.com>
2020-09-15 21:15:11 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,31 +1,31 @@
 class Artist
   attr_accessor :name
 
-  @@song_count = 0
+  @@all = []
 
   def initialize(name)
     @name = name
-    @songs = [ ]
+    @@all << self
   end
 
-  def self.song_count
-    @@song_count
+  def self.all
+    @@all
+  end
+
+  def add_song(song)
+    song.artist = self
+  end
+
+  def add_song_by_name(name)
+    song = Song.new(name)
+    song.artist = self
   end
 
   def songs
-    @songs
+    Song.all.select {|song| song.artist == self}
   end
 
-  def add_song(song_name)
-    @songs << song_name
-    song_name.artist = self
-    @@song_count += 1
+  def self.song_count
+    Song.all.count
   end
-
-  def add_song_by_name(song_name)
-    song = Song.new(song_name)
-    @songs << song
-    song.artist = self
-    @@song_count += 1
-  end
-end 
+end
